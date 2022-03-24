777ec0a4ce4ffdd50ec7f741ffbceddf5f970adc
Mizzza54 <gerasimov.misha2001@gmail.com>
2022-03-25 02:57:42 +0300

    Stage 2

---
+++ Stage2/ru.ifmo.gerasimov.client/src/main/java/ru/ifmo/gerasimov/client/Client.java
@@ -0,0 +1,25 @@
+package ru.ifmo.gerasimov.client;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import ru.ifmo.gerasimov.Greeting;
+
+/**
+ * @author Michael Gerasimov
+ */
+public class Client implements BundleActivator {
+
+    @Override
+    public void start(BundleContext bundleContext) throws Exception {
+        ServiceReference<?> ref = bundleContext.getServiceReference(Greeting.class.getName());
+        if (ref == null) {
+            System.out.println("Dont have");
+            return;
+        }
+        ((Greeting) bundleContext.getService(ref)).sayHello();
+    }
+
+    @Override
+    public void stop(BundleContext bundleContext) throws Exception {}
+}
